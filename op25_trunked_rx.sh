728b3778916ae7b3f47981a1513dce5f478e124b
Eric Tendian <erictendian@gmail.com>
2017-07-31 00:13:22 -0500

    Fixing control frequency bug in trunking script

--- op25_trunked_rx.sh
+++ op25_trunked_rx.sh
@@ -10,11 +10,11 @@ fi
 source .env
 
 TRUNKCONFIG=`readlink -f $BASEPATH/trunk.tsv`
-CONTROLFREQ=`grep $1 $TRUNKCONFIG | cut -f2 | sed 's/"//g'`
+CONTROLFREQ=`grep $1 $TRUNKCONFIG | cut -f2 | sed 's/"//g'`"e6"
 
 $TERMINAL -e "$BASEPATH/$AUDIO_SCRIPT"
 
 cd $RXDIR
-./rx.py  --args $OP25_ARGS --gains $OP25_GAIN -f "$CONTROLFREQ"e6 -q $OP25_PPM -S $OP25_SAMPLERATE -D $OP25_DEMOD -2 -o 12.5e3 -T $TRUNKCONFIG -w 2> $ERRORLOG
+./rx.py  --args $OP25_ARGS --gains $OP25_GAIN -f $CONTROLFREQ -q $OP25_PPM -S $OP25_SAMPLERATE -D $OP25_DEMOD -2 -o 12.5e3 -T $TRUNKCONFIG -w 2> $ERRORLOG
 kill $(pgrep "$AUDIO_SCRIPT")
 cat $ERRORLOG
